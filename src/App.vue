60c5ee71b62612f2f3348dec321cd5366f0aa1ad
huangjianzhuang <1554602253@qq.com>
2021-04-10 23:10:15 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import 'assets/css/base.css';
+
+
+</style>
